9daaee47a52df364254800f6749d3ccea66f49df
Eric Fabroa <eric.fabroa@ycdsbk12.ca>
2021-12-08 19:12:29 +0000

    Update

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import processing.core.PApplet;
 
 class Main {
   public static void main(String[] args) {
-    //PApplet.main("Sketch", args);
+    
     String[] processingArgs = {"MySketch"};
 	  Sketch mySketch = new Sketch();
 	  PApplet.runSketch(processingArgs, mySketch);
