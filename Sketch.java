de18e3d6f4b68ce630cd4d9a5a3599600ec3fae0
itsjackl <jack.lai25@ycdsbk12.ca>
2024-02-22 22:24:04 -0500

    Made a house

--- Sketch.java
+++ Sketch.java
@@ -16,7 +16,7 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(135, 206, 235);
   }
 
   /**
@@ -24,13 +24,28 @@ public class Sketch extends PApplet {
    */
   public void draw() {
 	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+	// Main part of the house
+  fill(196, 164, 132);
+  rect(100, 200, 200, 200);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+  // Roof of the house
+  fill(90, 90, 90);
+  triangle(100, 200, 300, 200, 200, 150);
+
+  // Windows of the houe
+  fill(53, 126, 199);
+  ellipse(150, 275, 50, 50);
+  ellipse(250, 275, 50, 50);
+  fill(0,0,0);
+  line(125, 275, 175, 275);
+  line(150, 250, 150, 300);
+  line(225, 275, 275, 275);
+  line(250, 250, 250, 300);
+
+  // Door of the house
+  fill(92, 64, 51);
+  rect(175, 300, 50, 100);
   }
   
-  // define other methods down here.
+  
 }
